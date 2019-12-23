32204f4d21c0197d853106b889a720c8315873ed
rtest1986 <56508765+rtest1986@users.noreply.github.com>
2019-12-23 22:26:50 +0100

    Update ability.rb

--- app/models/admin/ability.rb
+++ app/models/admin/ability.rb
@@ -18,10 +18,11 @@ module Admin
       can :menu, Deposit
       can :manage, ::Deposits::Bank
       can :manage, ::Deposits::Satoshi
-
+      can :manage, ::Deposits::Ozeety
       can :menu, Withdraw
       can :manage, ::Withdraws::Bank
       can :manage, ::Withdraws::Satoshi
+      can :manage, ::Deposits::Ozeety
     end
   end
 end
